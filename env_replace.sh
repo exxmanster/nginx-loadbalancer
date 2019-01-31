7a0b5e00d5cc52dacdf498cf8d334b154b9ec2d4
exxmanster <exxmanster@gmail.com>
2019-01-31 12:06:30 +0200

    add an ENV check

--- env_replace.sh
+++ env_replace.sh
@@ -1,8 +1,59 @@
 #/bin/bash
 
+
+
+########## ENV Check ##########
+if test -z "$NGINX_PORT" 
+then
+      echo "\$NGINX_PORT ENV is not define! ERORR!!!"
+else
+      echo "\$NGINX_PORT ENV is OK!"
+fi
+
+if test -z "$PROXY_PASS" 
+then
+      echo "\$PROXY_PASS ENV is not define! ERORR!!!"
+else
+      echo "\$PROXY_PASS ENV is OK!"
+fi
+
+if test -z "$PROXY_HOST1 " 
+then
+      echo "\$PROXY_HOST1 ENV is not define! ERORR!!!"
+else
+      echo "\$PROXY_HOST1 ENV is OK!"
+fi
+
+if test -z "$PROXY_HOST2" 
+then
+      echo "\$PROXY_HOST2 ENV is not define! ERORR!!!"
+else
+      echo "\$PROXY_HOST2 ENV is OK!"
+fi
+
+if test -z "$PROXY_HOST3" 
+then
+      echo "\$PROXY_HOST3 ENV is not define! ERORR!!!"
+else
+      echo "\$PROXY_HOST3 ENV is OK!"
+fi
+
+if test -z "$PROXY_HOST4" 
+then
+      echo "\$PROXY_HOST4 ENV is not define! ERORR!!!"
+else
+      echo "\$PROXY_HOST4 ENV is OK!"
+fi
+
+if test -z "$PROXY_HOST5" 
+then
+      echo "\$PROXY_HOST5 ENV is not define! ERORR!!!"
+else
+      echo "\$PROXY_HOST5 ENV is OK!"
+fi
 sleep 1
 
-echo build starting nginx config
+echo build starting NGINX config
 
 
 echo replacing ___NGINX_PORT___/$NGINX_PORT
@@ -26,13 +77,5 @@ sed -i "s/___PROXY_HOST3___/$PROXY_HOST3/g" /etc/nginx/nginx.conf
 sed -i "s/___PROXY_HOST4___/$PROXY_HOST4/g" /etc/nginx/nginx.conf
 sed -i "s/___PROXY_HOST5___/$PROXY_HOST5/g" /etc/nginx/nginx.conf
 
-
-### Checking part
-cat /etc/nginx/nginx.conf
-
-# if [ -z "$NGINX_PORT" ]; then
-#     echo "Need to set NGINX_PORT"
-#     exit 1
-# fi  
 ### Nginx start
 nginx -g 'daemon off;'
