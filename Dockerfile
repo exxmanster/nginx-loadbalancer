38bc177012ca19c460904074170e9f3ca7c47c86
exxmanster <exxmanster@gmail.com>
2019-02-06 13:51:33 +0200

    v2

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,14 @@
 FROM alpine:3.8
 
 # Install nginx and remove cache  
-RUN apk add --update nginx && rm -rf /var/cache/apk/*
-
-### You can uncoment this part and set your values here!!! ####
-# ENV NGINX_PORT 8888
-# ENV PROXY_PASS 192.168.88.59
-# ENV PROXY_HOST1 192.168.88.61:10001
-# ENV PROXY_HOST2 192.168.88.61:10002
-# ENV PROXY_HOST3 192.168.88.61:10003
-# ENV PROXY_HOST4 192.168.88.61:10004
-# ENV PROXY_HOST5 192.168.88.61:10005
-
+RUN apk add --update nginx vim && rm -rf /var/cache/apk/*
 
 # Copy files
-COPY load_balancer.conf /etc/nginx/nginx.conf
+COPY load_balancer_v2.conf /etc/nginx/nginx.conf
 COPY env_replace.sh /env_replace.sh
 
 # Make script executable and launching all stuff 
-ENTRYPOINT chmod a+x env_replace.sh && ./env_replace.sh
+ENTRYPOINT chmod a+x env_replace.sh && ./env_replace.sh ; sh
 
 
 
@@ -26,5 +16,17 @@ ENTRYPOINT chmod a+x env_replace.sh && ./env_replace.sh
 ######################## Debug ####################
 ###################################################
 #CMD ["nginx", "-g", "daemon off;"] 
-#ENTRYPOINT [ "sh" ]
+#ENTRYPOINT chmod a+x env_replace.sh
 #EXPOSE 8888
+### You can uncoment this part and set your values here!!! ####
+ENV NGINX_PORT 8888
+ENV PROXY_PASS1 http://192.168.88.59
+ENV PROXY_PASS2 http://192.168.88.59
+ENV PROXY_PASS3 http://192.168.88.59
+#ENV PROXY_PASS4 http://192.168.88.59
+ENV PROXY_PASS5 http://192.168.88.59
+ENV PROXY_HOST1 192.168.88.61:10001
+ENV PROXY_HOST2 192.168.88.61:10002
+ENV PROXY_HOST3 192.168.88.61:10003
+#ENV PROXY_HOST4 192.168.88.61:10004
+#ENV PROXY_HOST5 192.168.88.61:10005
--- env_replace.sh
+++ env_replace.sh
@@ -1,63 +1,19 @@
-#/bin/bash
 
-
-
-########## ENV Check ##########
-if test -z "$NGINX_PORT" 
-then
-      echo "\$NGINX_PORT ENV is not define! ERORR!!!"
-else
-      echo "\$NGINX_PORT ENV is OK!"
-fi
-
-if test -z "$PROXY_PASS" 
-then
-      echo "\$PROXY_PASS ENV is not define! ERORR!!!"
-else
-      echo "\$PROXY_PASS ENV is OK!"
-fi
-
-if test -z "$PROXY_HOST1" 
-then
-      echo "\$PROXY_HOST1 ENV is not define! ERORR!!!"
-else
-      echo "\$PROXY_HOST1 ENV is OK!"
-fi
-
-if test -z "$PROXY_HOST2" 
-then
-      echo "\$PROXY_HOST2 ENV is not define! ERORR!!!"
-else
-      echo "\$PROXY_HOST2 ENV is OK!"
-fi
-
-if test -z "$PROXY_HOST3" 
-then
-      echo "\$PROXY_HOST3 ENV is not define! ERORR!!!"
-else
-      echo "\$PROXY_HOST3 ENV is OK!"
-fi
-
-if test -z "$PROXY_HOST4" 
-then
-      echo "\$PROXY_HOST4 ENV is not define! ERORR!!!"
-else
-      echo "\$PROXY_HOST4 ENV is OK!"
-fi
-
-if test -z "$PROXY_HOST5" 
-then
-      echo "\$PROXY_HOST5 ENV is not define! ERORR!!!"
-else
-      echo "\$PROXY_HOST5 ENV is OK!"
-fi
-sleep 1
 
 echo build starting NGINX config
+echo
 
-
+### Check NGINX PORT
 echo replacing ___NGINX_PORT___/$NGINX_PORT
-echo replacing ___PROXY_PASS___/$PROXY_PASS
+
+### Check PROXY_PASS
+echo replacing ___PROXY_PASS1___/${PROXY_PASS1}
+echo replacing ___PROXY_PASS2___/${PROXY_PASS2}
+echo replacing ___PROXY_PASS3___/${PROXY_PASS3}
+echo replacing ___PROXY_PASS4___/${PROXY_PASS4}
+echo replacing ___PROXY_PASS5___/${PROXY_PASS5}
+
+### Check PROXY_HOST 
 echo replacing ___PROXY_HOST1___/$PROXY_HOST1
 echo replacing ___PROXY_HOST2___/$PROXY_HOST2
 echo replacing ___PROXY_HOST3___/$PROXY_HOST3
@@ -65,17 +21,31 @@ echo replacing ___PROXY_HOST4___/$PROXY_HOST4
 echo replacing ___PROXY_HOST5___/$PROXY_HOST5
 
 ### Nginx port replacment part
-sed -i "s/___NGINX_PORT___/$NGINX_PORT/g" /etc/nginx/nginx.conf
+    sed -i "s?___NGINX_PORT___?$NGINX_PORT?g" /etc/nginx/nginx.conf
 
 ### Proxy pass and upstream name replacment part 
-sed -i "s/___PROXY_PASS___/$PROXY_PASS/g" /etc/nginx/nginx.conf
+sed -i "s?___PROXY_PASS1___?${PROXY_PASS1}?g" /etc/nginx/nginx.conf
+sed -i "s?___PROXY_PASS2___?${PROXY_PASS2}?g" /etc/nginx/nginx.conf
+sed -i "s?___PROXY_PASS3___?${PROXY_PASS3}?g" /etc/nginx/nginx.conf
+sed -i "s?___PROXY_PASS4___?${PROXY_PASS4}?g" /etc/nginx/nginx.conf
+sed -i "s?proxy_pass" ""/"'___PROXY_PASS5___'?proxy_pass" ""/"'${PROXY_PASS5}'?g" /etc/nginx/nginx.conf
 
 ### Server part replace 
-sed -i "s/___PROXY_HOST1___/$PROXY_HOST1/g" /etc/nginx/nginx.conf
-sed -i "s/___PROXY_HOST2___/$PROXY_HOST2/g" /etc/nginx/nginx.conf
-sed -i "s/___PROXY_HOST3___/$PROXY_HOST3/g" /etc/nginx/nginx.conf
-sed -i "s/___PROXY_HOST4___/$PROXY_HOST4/g" /etc/nginx/nginx.conf
-sed -i "s/___PROXY_HOST5___/$PROXY_HOST5/g" /etc/nginx/nginx.conf
+sed -i "s?___PROXY_HOST1___?$PROXY_HOST1?g" /etc/nginx/nginx.conf
+sed -i "s?___PROXY_HOST2___?$PROXY_HOST2?g" /etc/nginx/nginx.conf
+sed -i "s?___PROXY_HOST3___?$PROXY_HOST3?g" /etc/nginx/nginx.conf
+sed -i "s?___PROXY_HOST4___?$PROXY_HOST4?g" /etc/nginx/nginx.conf
+sed -i "s?___PROXY_HOST5___?$PROXY_HOST5?g" /etc/nginx/nginx.conf
 
+
+
+###Debug
+echo
+echo
+echo
+cat /etc/nginx/nginx.conf
+echo
+echo
+echo
 ### Nginx start
 nginx -g 'daemon off;'
